4fa6b513622fb4e0a66ae35aa8f0397ba81322f7
Anderson Oliveira <anderson@sfoliveira@gmail.com>
2020-04-28 21:30:10 -0300

    Update NotificadorEmail.java

--- src/main/java/com/artisapp/curso/notificacao/NotificadorEmail.java
+++ src/main/java/com/artisapp/curso/notificacao/NotificadorEmail.java
@@ -1,7 +1,6 @@
 package com.artisapp.curso.notificacao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
